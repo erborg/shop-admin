e51507d81613e8adec15b88c3e1297afe27f2db2
Erik Borg <e.borg1990@gmail.com>
2021-05-03 17:03:55 +0300

    Updates

---
+++ precache-manifest.9d14fe2501175920867c5fe36c089324.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445c0c039b90a5d1ac82218259544b09",
+    "url": "/shop-admin/index.html"
+  },
+  {
+    "revision": "820ee9f8d328c6ed9757",
+    "url": "/shop-admin/static/js/2.8ef85d98.chunk.js"
+  },
+  {
+    "revision": "2d1b5bb821e3938458a9",
+    "url": "/shop-admin/static/js/main.05db9365.chunk.js"
+  },
+  {
+    "revision": "7bb2f49f3196707fd7be",
+    "url": "/shop-admin/static/js/runtime~main.461e9e8f.js"
+  }
+]);
